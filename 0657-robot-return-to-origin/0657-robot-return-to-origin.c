7b2c5ba8d676fb4c4380b6ce2e160b5d4a4c5da9
Thien Tin <thientin31072006@gmail.com>
2026-03-11 11:48:29 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.c
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.c
@@ -1,9 +1,8 @@
-bool judgeCircle(char* moves) {
-    int size = strlen(moves);
+bool judgeCircle(char* moves) {;
     int cnt1 = 0;
     int cnt2 = 0;
 
-    for(int i = 0; i < size; i++){
+    for(int i = 0; moves[i]; i++){
         if(moves[i] == 'U') cnt1++;
         else if(moves[i] == 'D') cnt1--;
         else if(moves[i] == 'R') cnt2++;
