4a71d43fbeedbf66b334a1f577c3912bce663e09
Thien Tin <thientin31072006@gmail.com>
2026-03-12 20:45:52 +0700

    Time: 0 ms (100%), Space: 17.1 MB (11.36%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.c
@@ -0,0 +1,16 @@
+double findMaxAverage(int* nums, int numsSize, int k) {
+    int sum = 0;
+
+    for(int i = 0; i < k; i++){
+        sum += nums[i];
+    }
+
+    int maxSum = sum;
+    
+    for(int i = k; i < numsSize; i++){
+        sum = sum - nums[i - k] + nums[i];
+        maxSum = sum > maxSum ? sum : maxSum;
+    }
+
+    return (double)maxSum/k;
+}
