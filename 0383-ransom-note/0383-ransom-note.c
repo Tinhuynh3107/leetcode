a825223fa4aa26af0c663da5dadabb19df9f1ea7
Thien Tin <thientin31072006@gmail.com>
2026-03-23 10:16:34 +0700

    Time: 0 ms (100%), Space: 9.6 MB (15.61%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,15 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    int *map = calloc(256, sizeof(int));
+    for (int i = 0; ransomNote[i]; i++){
+        map[ransomNote[i]] += 1;
+    }
+
+    for (int i = 0; magazine[i]; i++){
+        map[magazine[i]] -= 1;
+    }
+
+    for (int i = 0; ransomNote[i]; i++){
+        if (map[ransomNote[i]] > 0) return 0;
+    }
+    return 1;
+}
