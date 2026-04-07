c5c9d5abd296767cfb868fb25a8175de4d60aacf
Thien Tin <thientin31072006@gmail.com>
2026-04-07 13:15:39 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.c
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.c
@@ -22,7 +22,6 @@ struct TreeNode* sortedListToBST(struct ListNode* head) {
         return node;
     }
 
-    // Tìm node giữa bằng slow/fast pointer
     struct ListNode* slow = head;
     struct ListNode* fast = head->next->next;
     while (fast && fast->next) {
@@ -30,13 +29,12 @@ struct TreeNode* sortedListToBST(struct ListNode* head) {
         fast = fast->next->next;
     }
 
-    // slow->next là mid (root)
     struct ListNode* mid = slow->next;
-    slow->next = NULL; // Cắt đôi list
+    slow->next = NULL; 
 
     struct TreeNode* root = malloc(sizeof(struct TreeNode));
     root->val = mid->val;
-    root->left  = sortedListToBST(head);     // nửa trái
-    root->right = sortedListToBST(mid->next); // nửa phải
+    root->left  = sortedListToBST(head);    
+    root->right = sortedListToBST(mid->next); 
     return root;
 }
